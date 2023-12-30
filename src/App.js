90dda0c58b13fc5364c356cecf2aebf6d2abb694
lata bansavad <latabansavade24@gmail.com>
2023-12-30 16:48:27 +0530

    f4 contest 3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import FetchingData from "./Components/FetchingData";
+
+function App() {
+  return (
+    <div className="App">
+      <FetchingData/>
+    </div>
+  );
+}
+
+export default App;
